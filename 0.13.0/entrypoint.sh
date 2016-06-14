3c40e94df913088f71ff5b774e01befcfd084c1f
Christian Rost <chr@baltic-online.de>
2016-06-14 13:33:55 +0200

    update

--- 0.13.0/entrypoint.sh
+++ 0.13.0/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 set -e
 
-if [ "${1:0:1}" = '-' ]; then
-    set -- influxd "$@"
-fi
+# ensure the following environment variables are set. exit script and container if not set.
+#test $backend_host
+#test $load_balancer_domain
+#test $app_servers
+
+# run confd
+export CONFD_OPTIONS=${CONFD_OPTIONS:--backend env}
+/usr/local/bin/confd -onetime $CONFD_OPTIONS
 
 exec "$@"
